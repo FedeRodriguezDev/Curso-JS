fcbf351ee082160d2121943e30d0f5fe028b42f5
FedeRodriguezDev <federicodom_13@hotmail.com>
2024-07-30 23:17:38 -0300

    Reentrega

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
+const imcCategorias = [
+    { min: null, max: 18.4, categoria: "bajo peso" },
+    { min: 18.5, max: 24.9, categoria: "peso normal" },
+    { min: 25, max: 29.9, categoria: "sobrepeso" },
+    { min: 30, max: Infinity, categoria: "obesidad" }
+];
+
 let altura = parseFloat(prompt("Ingresa tu altura en centímetros."));
 if (altura <= 0) {
     alert("Por favor, ingresa una altura válida en centímetros.");
@@ -9,6 +16,8 @@ if (altura <= 0) {
     } else {
         let imc = calcularIMC(peso, altura);
         console.log("Tu IMC es: " + imc.toFixed(2));
+console.log('Ese valor representa: ' + obtenerCategoria(imc))
+        
     }
 }
 
@@ -16,4 +25,14 @@ if (altura <= 0) {
 function calcularIMC(peso, altura) {
     altura = altura / 100;
     return peso / (altura * altura);
+}
+
+// Función para obtener la categoría basada en un IMC dado
+function obtenerCategoria(imc) {
+    for (const categoria of imcCategorias) {
+        if ((imc >= categoria.min) && (imc <= categoria.max)) {
+            return categoria.categoria;
+        }
+    }
+    return "Categoría desconocida";
 }
